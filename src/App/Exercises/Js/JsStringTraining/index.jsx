978c0696c58dd1ad57e3e93208099301a47b8a48
Anna Ochocka <annoch22@gmail.com>
2023-07-25 22:32:54 +0200

    Poprawki

---
+++ src/App/Exercises/Js/JsStringTraining/index.jsx
@@ -0,0 +1,40 @@
+import './styles.css';
+import TextTruncate from 'react-text-truncate'; // recommend
+
+export function JsStringTraining() {}
+
+// 5.1
+function titleCase(str) {
+  var arr = str.split(' ');
+  for (var i = 0; i < arr.length; i++) {
+    arr[i] = arr[i].split('');
+    for (var x = 0; x < 1; x++) {
+      arr[i][x] = arr[i][x].toUpperCase();
+      console.log(arr);
+    }
+    for (var y = 1; y < arr[i].length; y++) {
+      arr[i][y] = arr[i][y].toLowerCase();
+      console.log(arr);
+    }
+    arr[i] = arr[i].join('');
+  }
+  arr = arr.join('  ');
+  console.log(arr);
+}
+
+titleCase('anna ma psa');
+
+/* let titleCase = (str) => str.split(" ").map((str) => str = str.charAt(0).toUpperCase() + str.substring(1).toLowerCase()).join(" ");
+console.log(titleCase("Jakiś dłuŻszy PrzYkŁaDowy tekst")); */
+
+//5.2
+
+<TextTruncate
+  line={1}
+  element="span"
+  truncateText="…"
+  text="Lorem Ipsum is simply dummy text of the printing and typesetting industry."
+  textTruncateChild={<a href="#">Read on</a>}
+/>;
+
+// 5.3

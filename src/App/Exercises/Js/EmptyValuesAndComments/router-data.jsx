e7859aabcac64124267132c5def91baba7f652f4
Anna Ochocka <annoch22@gmail.com>
2023-07-25 20:07:34 +0200

    empty values

---
+++ src/App/Exercises/Js/EmptyValuesAndComments/router-data.jsx
@@ -0,0 +1,10 @@
+import { EmptyValues } from './index';
+
+export const EmptyValuesMetaData = {
+  path: 'Empty-Values',
+  date: null,
+  linkLabel: 'Empty values and comments',
+  blockNo: 6,
+  element: <EmptyValues />,
+  tags: ['array', 'map', 'filter', 'flex'],
+};

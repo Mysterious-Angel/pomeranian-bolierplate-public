04908e64a58d9413062556630a680021b0c42d87
Anna Ochocka <129973467+Mysterious-Angel@users.noreply.github.com>
2023-08-31 23:57:21 +0200

    Update firebase.js

--- src/App/Exercises/ReactExercises/FireBaseLoginForm/firebase.js
+++ src/App/Exercises/ReactExercises/FireBaseLoginForm/firebase.js
@@ -1,12 +1,7 @@
 import { initializeApp } from 'firebase/app';
 
 const firebaseConfig = {
-  apiKey: 'AIzaSyChug8a7Q_q4J7YGptIhTBUFPGvnx3pKv4',
-  authDomain: 'pomeranian-firebase-1995f.firebaseapp.com',
-  projectId: 'pomeranian-firebase-1995f',
-  storageBucket: 'pomeranian-firebase-1995f.appspot.com',
-  messagingSenderId: '141160858650',
-  appId: '1:141160858650:web:383b9d6a7c51725d29fda3',
+  
 };
 
 export const app = initializeApp(firebaseConfig);
